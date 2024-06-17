134dbd6f25696e40db8b9e245e45d2674ba06168
vadik likholetov <vadikas@gmail.com>
2024-06-17 14:55:20 +0300

    initial

---
+++ configuration.nix
@@ -0,0 +1,255 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "mitigations=off" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+  ];
+
+  # Enable networking
+  networking = {
+    nameservers = [ "127.0.0.1" "::1" ];
+    # If using dhcpcd:
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    # If using NetworkManager:
+    networkmanager.enable = true;
+    networkmanager.dns = "none";
+    hostName = "woooster"; # Define your hostname.
+  };
+
+  virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [ "kvm-intel" ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fi_FI.UTF-8";
+    LC_IDENTIFICATION = "fi_FI.UTF-8";
+    LC_MEASUREMENT = "fi_FI.UTF-8";
+    LC_MONETARY = "fi_FI.UTF-8";
+    LC_NAME = "fi_FI.UTF-8";
+    LC_NUMERIC = "fi_FI.UTF-8";
+    LC_PAPER = "fi_FI.UTF-8";
+    LC_TELEPHONE = "fi_FI.UTF-8";
+    LC_TIME = "fi_FI.UTF-8";
+  };
+
+  # Enable the GNOME Desktop Environment.
+  # Ensure gnome-settings-daemon udev rules are enabled.
+  services.udev.packages = with pkgs; [
+    gnome.gnome-settings-daemon
+    yubikey-personalization
+  ];
+  services.pcscd.enable = true;
+  
+  services.gnome.gnome-keyring.enable = lib.mkForce false;
+
+   environment.shellInit = ''
+     gpg-connect-agent /bye
+     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+   '';
+
+
+  services.xserver = {
+    # Required for DE to launch.
+    enable = true;
+    displayManager = {
+      gdm = {
+        enable = true;
+        wayland = true;
+      };
+    };
+    # Enable Desktop Environment.
+    desktopManager.gnome.enable = true;
+    excludePackages = with pkgs; [ xterm ];
+  };
+
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  services.stubby = {
+    enable = true;
+    settings = pkgs.stubby.passthru.settingsExample // {
+      upstream_recursive_servers = [{
+        address_data = "1.1.1.1";
+        tls_auth_name = "cloudflare-dns.com";
+        tls_pubkey_pinset = [{
+          digest = "sha256";
+          value = "HdDBgtnj07/NrKNmLCbg5rxK78ZehdHZ/Uoutx4iHzY=";
+        }];
+      }
+        {
+          address_data = "1.0.0.1";
+          tls_auth_name = "cloudflare-dns.com";
+          tls_pubkey_pinset = [{
+            digest = "sha256";
+            value = "HdDBgtnj07/NrKNmLCbg5rxK78ZehdHZ/Uoutx4iHzY=";
+          }];
+        }];
+    };
+  };
+
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vadikas = {
+    isNormalUser = true;
+    description = "vadikas";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      gnome.gnome-tweaks
+      gnomeExtensions.appindicator
+      gnomeExtensions.caffeine
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.just-perfection
+      gnomeExtensions.window-title-is-back
+      firefox
+      telegram-desktop
+      zapzap
+      slack
+      git
+      sublime4
+      nixpkgs-fmt
+      mc
+      virtualenv
+      vagrant
+      yubioath-flutter
+      terminator
+      devenv
+    ];
+  };
+
+ environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    eog         # image viewer
+    epiphany    # web browser
+    simple-scan # document scanner
+    totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    file-roller # archive manager
+    geary       # email client
+    seahorse    # password manager
+
+    gnome-calculator
+    gnome-calendar
+    gnome-characters
+    gnome-clocks
+    gnome-contacts
+    gnome-font-viewer
+    gnome-logs
+    gnome-maps
+    gnome-music
+    gnome-screenshot
+    gnome-weather
+    gnome-disk-utility
+    pkgs.gnome-connections
+  ];
+
+
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Allow broken/unsecure packages
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1w"
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+    htop
+    ripgrep
+    usbutils
+    pciutils
+    python3
+    gnupg
+    yubikey-personalization
+    screen
+    microcom
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  
+  # Enable ZeroTier
+  services.zerotierone.enable = true;
+  services.zerotierone.joinNetworks = [ "17d709436ccd20b1" ];
+  
+  # Enable fwupdate
+  services.fwupd.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
